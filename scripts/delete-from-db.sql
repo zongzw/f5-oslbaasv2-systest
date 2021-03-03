9447485f5640ec4f9b7ed8bf3f78f0b56885a6e7
Andrew Zong <a.zong@f5.com>
2021-03-03 18:14:11 +0800

    delete allocated ip addresses.

--- scripts/delete-from-db.sql
+++ scripts/delete-from-db.sql
@@ -14,4 +14,6 @@ delete from lbaas_loadbalancer_statistics;
 delete from lbaas_loadbalancers;
 delete from lbaas_loadbalanceragentbindings;
 
-delete from ports where device_owner = 'neutron:LOADBALANCERV2' or device_owner = 'network:f5lbaasv2';
+delete from ports where device_owner in ('neutron:LOADBALANCERV2', 'network:f5lbaasv2', 'F5:lbaasv2');
+
+delete from ipamallocations where ip_address not in (select ip_address from ipallocations);

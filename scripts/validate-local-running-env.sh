5aa36d97ae30e9788f2e624d78cf29ca1873e07e
Andrew Zong <a.zong@f5.com>
2021-03-03 21:02:57 +0800

    Reimplement mtls with ansible yaml instead of shell-almostly scripts.

---
+++ scripts/validate-local-running-env.sh
@@ -0,0 +1,13 @@
+# TODO: validate ansible running environment on localhost
+
+# 1. validate ansible installed.
+
+# 1.1 validate openssl is installed.
+
+# 2. check hosts' connection and setup password-less accessing
+
+# 3. install necessary collections:
+#   ansible-galaxy collection install f5networks.f5_modules: 
+#       https://docs.ansible.com/ansible/latest/collections/f5networks/f5_modules/
+#   ansible-galaxy collection install community.crypto
+#       https://docs.ansible.com/ansible/latest/collections/community/crypto/

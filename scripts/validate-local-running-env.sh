50c5812303d47a23b7e01bc007e11ba69e694216
Andrew Zong <a.zong@f5.com>
2021-03-04 12:52:33 +0800

    some little comments as future works.

--- scripts/validate-local-running-env.sh
+++ scripts/validate-local-running-env.sh
@@ -2,11 +2,13 @@
 
 # 1. validate ansible installed.
 
-# 1.1 validate openssl is installed.
+# 2. validate openssl is installed.
 
-# 2. check hosts' connection and setup password-less accessing
+# 3. generate keypairs for tls related test.
 
-# 3. install necessary collections:
+# 4. check hosts' connection and setup password-less accessing
+
+# 5. install necessary collections:
 #   ansible-galaxy collection install f5networks.f5_modules: 
 #       https://docs.ansible.com/ansible/latest/collections/f5networks/f5_modules/
 #   ansible-galaxy collection install community.crypto

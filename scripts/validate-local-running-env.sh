ac773610206a97a863fd9f92244224e4e6fb40cc
Andrew Zong <a.zong@f5.com>
2021-03-05 21:53:03 +0800

    check app_cookie/source_ip/http_cookie persistence effect.

--- scripts/validate-local-running-env.sh
+++ scripts/validate-local-running-env.sh
@@ -1,14 +1,12 @@
 # TODO: validate ansible running environment on localhost
 
-# 1. validate ansible installed.
+# 1. validate installation: ansible openssl git
 
-# 2. validate openssl is installed.
+# 2. generate keypairs for tls related test.
 
-# 3. generate keypairs for tls related test.
+# 3. check hosts' connection and setup password-less accessing
 
-# 4. check hosts' connection and setup password-less accessing
-
-# 5. install necessary collections:
+# 4. install necessary collections:
 #   ansible-galaxy collection install f5networks.f5_modules: 
 #       https://docs.ansible.com/ansible/latest/collections/f5networks/f5_modules/
 #   ansible-galaxy collection install community.crypto

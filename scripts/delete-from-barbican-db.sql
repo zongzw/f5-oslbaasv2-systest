5bd9d49196d5c679f5f59dd2f0635dde08e9164f
Andrew Zong <a.zong@f5.com>
2021-03-11 11:39:46 +0800

    clear barbican certificates

---
+++ scripts/delete-from-barbican-db.sql
@@ -0,0 +1,11 @@
+
+delete from container_consumer_metadata;
+delete from container_secret;
+delete from containers;
+
+delete from encrypted_data;
+delete from secret_store_metadata;
+delete from secrets;
+
+delete from kek_data;
+delete from projects;

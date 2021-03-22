bf071f3ac90bd6bb2dfcd2a148686f2aa380d11e
jinshuo <zhanghang080@sina.cn>
2021-03-22 11:33:46 +0800

    consider using get_url module

--- scripts/run-tests-parallel.py
+++ scripts/run-tests-parallel.py
@@ -1,1 +1,11 @@
 # TODO: run all tests in parallel to save time.
+from ansible.cli.playbook import PlaybookCLI as pbc
+ipath = r"/Users/shjin/Desktop/mytest/ansbgthub/f5-oslbaasv2-systest/conf.d/group_and_hosts-dev"
+testpath = r"/Users/shjin/Desktop/mytest/ansbgthub/f5-oslbaasv2-systest/testcases"
+def main():
+    
+    cli = pbc(["","-i",ipath,testpath+r"/dev_test.yml"])
+    exitcode = cli.run()
+
+if(__name__ == "__main__"):
+    main()

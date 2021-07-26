2f1a992bd6136dc6b6656e3658cdc10b07d21a35
Andrew ZONG <a.zong@f5.com>
2021-07-26 10:25:49 +0800

    little update to docker.d

--- systest-docker/compose.sh
+++ systest-docker/compose.sh
@@ -1,2 +1,2 @@
 export HOMEDIR=`cd $(dirname $0); pwd`
-docker-compose -f $HOMEDIR/docker-compose.yml up -d  --remove-orphans 
+docker-compose -f $HOMEDIR/docker-compose.yml up -d --force-recreate --remove-orphans 
